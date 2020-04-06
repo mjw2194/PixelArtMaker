48e7ac3f900c38b1d3d109f3caa2f8a450b491e3
Max Williams <max.williams@ccmllc.com>
2020-04-06 16:21:13 -0700

    added mpv capture dimensions function

--- designs.js
+++ designs.js
@@ -2,7 +2,12 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
+function captureDimensions(event){
+  var height = document.getElementById('inputHeight');
+  var width = document.getElementById('inputWidth');
+  var dimensions = [height,width];
+  return dimensions;
+}
 function makeGrid() {
 
 // Your code goes here!
