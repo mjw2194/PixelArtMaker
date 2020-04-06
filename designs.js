25af4b66f202b38ce9a91e7287f2a386e8dc53d2
Max Williams <max.williams@ccmllc.com>
2020-04-06 16:43:17 -0700

    v0.0.002 printing dimensions to console: tied to click listener

--- designs.js
+++ designs.js
@@ -2,12 +2,22 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-function captureDimensions(event){
+
+captureDimensions = function (event){
   var height = document.getElementById('inputHeight');
   var width = document.getElementById('inputWidth');
-  var dimensions = [height,width];
+  var dimensions = [height.value,width.value];
   console.log(dimensions);
 }
+
+hearThat = function() {
+  console.log("CLICK CLACK!!!");
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  document.addEventListener('click', captureDimensions);
+});
+
 function makeGrid() {
 
 // Your code goes here!
