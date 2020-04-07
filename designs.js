cc4dec11bf7878c1144fd6b3cd9a0f21b1c8486d
Max Williams <max.williams@ccmllc.com>
2020-04-06 22:14:24 -0700

    v0.0.0035 tried a bunch of shit

--- designs.js
+++ designs.js
@@ -3,36 +3,47 @@
 
 // When size is submitted by the user, call makeGrid()
 
-captureDimensions = function (event){
-  var height = document.getElementById('inputHeight');
-  var width = document.getElementById('inputWidth');
-  var dimensions = [height.value,width.value];
-  console.log(dimensions);
+hearThat = function() {
+  console.log("CLICK CLACK!!!");
 }
+
+
+captureDimensions = function (){
+  var height = document.getElementById('inputHeight').value;
+  var width = document.getElementById('inputWidth').value;
+  return dimensions = [parseInt(height, 10), parseInt(width, 10)];
+  //console.log(dimensions);
+}
+
 
 captureColor = function(event){
   var color = document.getElementById('colorPicker');
   console.log(color.value);
 }
 
-hearThat = function() {
-  console.log("CLICK CLACK!!!");
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-  document.addEventListener('click', captureDimensions);
-  document.addEventListener('click', captureColor);
-});
 
 function makeGrid() {
 
-// Your code goes here!
-  for (var row = 1; row <=4; row++){
-    var canvass = document.getElementById('pixelCanvas');
-    canvass.insertRow()
-    for (var col = 1; col <=4; col++){
+  //var height = captureDimensions()[0];
+  //var width = captureDimensions()[1];
+  var height = document.getElementById('inputHeight').value;
+  var width = document.getElementById('inputWidth').value;
+  var canvass = document.getElementById('pixelCanvas');
+
+  for (var row = 1; row <=width; row++){
+    canvass.insertRow();
+    for (var col = 1; col <=width; col++){
       canvass.rows[row - 1].insertCell();
     }
   }
-
 }
+
+
+var picker = document.getElementById('sizePicker')
+picker.addEventListener('submit', makeGrid);
+
+
+/*paint = function(event){
+  var pixel = document.getElements
+}
+*/
