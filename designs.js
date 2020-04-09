4351691d7e2558b953969fc805acea024d9fc11f
Max Williams <max.williams@ccmllc.com>
2020-04-08 21:16:41 -0700

    cleaned up js per Udacity recommendations

--- designs.js
+++ designs.js
@@ -8,19 +8,19 @@ Contents
 
 //1: Trivial event for testing purposes
 hearThat = function(event) {
-  console.log(event.target + "CLICK CLACK!!!");
-}
+  console.log(event.target + 'CLICK CLACK!!!');
+};
 
 
 //2: set color of target table elements
 setColor = function(event){
   //capture color for color picker
-  var color = document.getElementById('colorPicker').value;
+  const color = document.getElementById('colorPicker').value;
 
   //change class of target element once clicked which allows multi colors
   if (event.target.className === 'filled'){
     event.target.className = 'empty';
-    event.target.style.backgroundColor = "#ffffff";
+    event.target.style.backgroundColor = '#ffffff';
   }
   else {
     event.target.className = 'filled';
@@ -35,17 +35,17 @@ function makeGrid(event) {
   //prevent default to allow form submission and creation
   event.preventDefault();
   //capture relevant data and elements
-  var height = document.getElementById('inputHeight').value;
-  var width = document.getElementById('inputWidth').value;
-  var canvass = document.getElementById('pixelCanvas');
-  var new_row = document.createElement('tr');
+  const height = document.getElementById('inputHeight').value;
+  const width = document.getElementById('inputWidth').value;
+  const canvass = document.getElementById('pixelCanvas');
+  const new_row = document.createElement('tr');
 
   //resets canvas if table already exists
   canvass.innerHTML = null;
 
   //creates table with user defined rows and coloumns
   //HTML file edited to allow 25X25
-  for (var row = 1; row <=width; row++){
+  for (let row = 1; row <=width; row++){
     //for the first row, need to use append appendChild
     //afterwards insertrow will work fine
     if(row == 1){
@@ -54,7 +54,7 @@ function makeGrid(event) {
     else {
       canvass.insertRow();
     }
-    for (var col = 1; col <=width; col++){
+    for (let col = 1; col <=width; col++){
       canvass.rows[row - 1].insertCell();
     }
   }
